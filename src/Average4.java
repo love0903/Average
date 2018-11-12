74a5f6ef2b4c6cd98b8f78df3b020f9aa5460b4f
love0903 <a0958007246@gmail.com>
2018-11-12 21:50:10 +0800

    02

---
+++ src/Average4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Average4 {
+
+	public static void main(String[] args) {
+		double sum = 0;
+		int count = 0;
+		int number;
+		while(true) {
+			number = nextInt();
+			if(number ==0) {
+				break;
+			}
+			sum += number;
+			count++;
+			}
+			System.out.printf("平均 %.2f%n", sum / count);
+			}
+			static Scanner scanner = new Scanner(System.in);
+			static int nextInt() {
+			String input = scanner.next();
+			while(!input.matches("\\d*")) {
+			System.out.println("請輸入數字");
+			input = scanner.next();
+			}
+			return Integer.parseInt(input);
+			}
+			}
